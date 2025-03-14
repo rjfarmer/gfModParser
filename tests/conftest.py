48161520392154c67b8dc1318349b02e87f94884
Robert Farmer <robert.j.farmer37@gmail.com>
2025-03-14 21:47:19 +0000

    Readd shell=True

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,5 +4,5 @@ import subprocess
 
 
 def pytest_configure(config):
-    subprocess.call(["make", "clean"], cwd="tests")
-    subprocess.call(["make"], cwd="tests")
+    subprocess.call(["make", "clean"], shell=True, cwd="tests")
+    subprocess.call(["make"], shell=True, cwd="tests")
