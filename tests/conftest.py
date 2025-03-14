a2fb09c5c020e8812be3616985d35fcb6a054249
Robert Farmer <robert.j.farmer37@gmail.com>
2025-03-14 21:18:23 +0000

    Fix bandit issues

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,8 @@
 # SPDX-License-Identifier: GPL-2.0+
 
 import subprocess
-import pytest
-import _pytest.skipping
 
 
 def pytest_configure(config):
-    subprocess.call(["make", "clean"], shell=True, cwd="tests")
-    subprocess.call(["make"], shell=True, cwd="tests")
+    subprocess.call(["make", "clean"], cwd="tests")
+    subprocess.call(["make"], cwd="tests")
