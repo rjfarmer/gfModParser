e4b61aa3cb073d411ae92c242bcd0e5b2b64eae0
Robert Farmer <robert.j.farmer37@gmail.com>
2025-03-14 19:56:38 +0000

    Tests for typebound_proc

---
+++ tests/test_typebound_proc.py
@@ -0,0 +1,35 @@
+# SPDX-License-Identifier: GPL-2.0+
+
+import os
+import pytest
+from pprint import pprint
+
+import gfModParser as gf
+
+
+class TestTypeboundProc:
+    @pytest.fixture(autouse=True)
+    def load(self):
+        self.mod = gf.module("tests/build/proc_ptrs.mod")
+
+    def test_pass(self):
+        pp = self.mod["Ppptr"].properties.components["p_func_pass"].proc_pointer
+
+        assert pp.access == "PUBLIC"
+        assert pp.overridable == "OVERRIDABLE"
+        assert not pp.nopass
+        assert pp.is_generic == "SPECIFIC"
+        assert pp.ppc == "PPC"
+        assert pp.pass_arg == ""
+        assert pp.pass_arg_num > 0
+
+    def test_nopass(self):
+        pp = self.mod["Ppptr"].properties.components["p_func_func_run_ptr"].proc_pointer
+        assert pp.nopass
+        assert pp.pass_arg == ""
+        assert pp.pass_arg_num == 0
+
+    def test_pass_self(self):
+        pp = self.mod["Ppptr"].properties.components["p_func_pass2"].proc_pointer
+        assert not pp.nopass
+        assert pp.pass_arg_num > 0
