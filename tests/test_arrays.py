c2429b07ae3e5f7824d366b36175cdfdb676b399
Rob Farmer <robert.j.farmer37@gmail.com>
2025-03-15 20:16:24 +0000

    Fix array tests

--- tests/test_arrays.py
+++ tests/test_arrays.py
@@ -11,7 +11,6 @@ class TestArrays:
         self.explicit = gf.module("tests/build/explicit_arrays.mod")
         self.dummy = gf.module("tests/build/dummy_arrays.mod")
 
-    @property
     def test_rank(self):
         assert self.explicit["b_int_exp_1d"].properties.array_spec.rank == 1
         assert self.explicit["b_int_exp_2d"].properties.array_spec.rank == 2
@@ -19,7 +18,6 @@ class TestArrays:
         assert self.explicit["b_int_exp_4d"].properties.array_spec.rank == 4
         assert self.explicit["b_int_exp_5d"].properties.array_spec.rank == 5
 
-    @property
     def test_corank(self):
         assert self.explicit["b_int_exp_1d"].properties.array_spec.corank == 0
         assert self.explicit["b_int_exp_2d"].properties.array_spec.corank == 0
@@ -27,16 +25,10 @@ class TestArrays:
         assert self.explicit["b_int_exp_4d"].properties.array_spec.corank == 0
         assert self.explicit["b_int_exp_5d"].properties.array_spec.corank == 0
 
-    @property
     def test_type(self):
-        assert self.explicit["b_int_exp_1d"].properties.array_spec.type == "INTEGER"
-        assert self.explicit["b_real_exp_1d"].properties.array_spec.type == "REAL"
-        assert self.explicit["b_str_exp_1d"].properties.array_spec.type == "CHARACTER"
-        assert (
-            self.explicit["const_logical_arr"].properties.array_spec.type == "LOGICAL"
-        )
+        assert self.explicit["b_int_exp_1d"].properties.array_spec.type == "EXPLICIT"
+        assert self.dummy["c_str_alloc_1d"].properties.array_spec.type == "DEFERRED"
 
-    @property
     def test_if_array(self):
         assert self.explicit["const_int_arr"].properties.array_spec
         assert not self.explicit["dp"].properties.array_spec
