0951f28a9cb744c4d9df9908e2408fd04189f776
Robert Farmer <robert.j.farmer37@gmail.com>
2025-05-10 23:05:35 +0100

    Run pytest config only once

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,10 @@ import os
 
 
 def pytest_configure(config):
+    """This is run when pytest is setting up in the controller process and in the workers too"""
+    if hasattr(config, "workerinput"):
+        # prevent workers to run the same code
+        return
     try:
         os.mkdir(os.path.join("tests", "build"))
     except FileExistsError:
