f0e6cd8f06af23b499d42f4ea2868125169aa894
Rob Farmer <robert.j.farmer37@gmail.com>
2025-05-27 14:12:49 +0100

    Fix tests

--- tests/test_procedures.py
+++ tests/test_procedures.py
@@ -16,12 +16,12 @@ class TestProcedures:
     def test_ret_func(self):
         ref = self.mod["func_check_mod"].properties.symbol_reference
         assert ref > 0
-        assert mod[ref].name == "func_check_mod"
+        assert self.mod[ref].name == "func_check_mod"
 
     def test_ret_func_result(self):
         ref = self.mod["func_result"].properties.symbol_reference
         assert ref > 0
-        assert mod[ref].name == "z"
+        assert self.mod[ref].name == "z"
 
     def test_ret_sub(self):
         ref = self.mod["sub_int_in"].properties.symbol_reference
@@ -30,31 +30,31 @@ class TestProcedures:
     def test_args(self):
         refs = self.mod["func_intent_out"].properties.formal_argument
         assert len(refs) == 2
-        assert mod[refs[0]].name == "y"
-        assert mod[refs[1]].name == "x"
+        assert self.mod[refs[0]].name == "y"
+        assert self.mod[refs[1]].name == "x"
 
     def test_args_ret_type(self):
         ref = self.mod["func_check_mod"].properties.symbol_reference
-        arg = mod[ref]
+        arg = self.mod[ref]
         assert arg.properties.typespec.kind == 4
         assert arg.properties.typespec.type == "LOGICAL"
 
     def test_intent(self):
         ref = self.mod["func_result"].properties.symbol_reference
-        return_arg = mod[ref]
+        return_arg = self.mod[ref]
         assert return_arg.properties.attributes.intent == "UNKNOWN-INTENT"
 
         refs = self.mod["func_result"].properties.formal_argument
-        assert mod[refs[0]].properties.attributes.intent == "IN"
-        assert mod[refs[1]].properties.attributes.intent == "OUT"
+        assert self.mod[refs[0]].properties.attributes.intent == "IN"
+        assert self.mod[refs[1]].properties.attributes.intent == "OUT"
 
     def test_value(self):
         refs = self.mod["func_int_value"].properties.formal_argument
-        assert mod[refs[0]].properties.attributes.value
+        assert self.mod[refs[0]].properties.attributes.value
 
     def test_optional(self):
         refs = self.mod["sub_int_opt"].properties.formal_argument
-        assert mod[refs[0]].properties.attributes.optional
+        assert self.mod[refs[0]].properties.attributes.optional
 
     def test_case(self):
         assert self.mod["func_TEST_CASE"].properties.symbol_reference > 0
