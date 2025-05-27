36b03699a95104e4ad79dbb36cea03c014f7f10b
Rob Farmer <robert.j.farmer37@gmail.com>
2025-05-27 15:04:13 +0100

    Tabs to spaces

--- tests/src/elemental.f90
+++ tests/src/elemental.f90
@@ -2,77 +2,77 @@
 
 module elements
 
-	use iso_fortran_env, only: output_unit, real128
-	
-	implicit none
-	
-	! Parameters
-	integer, parameter :: dp = selected_real_kind(p=15)
-	integer, parameter :: qp = selected_real_kind(p=30)
-	integer, parameter :: lp = selected_int_kind(16)
-	
-	
-	contains
+    use iso_fortran_env, only: output_unit, real128
+    
+    implicit none
+    
+    ! Parameters
+    integer, parameter :: dp = selected_real_kind(p=15)
+    integer, parameter :: qp = selected_real_kind(p=30)
+    integer, parameter :: lp = selected_int_kind(16)
+    
+    
+    contains
 
 
-	elemental integer function ele_func_1(x)
-		integer,intent(in) :: x
+    elemental integer function ele_func_1(x)
+        integer,intent(in) :: x
 
-		ele_func_1 = x*2
+        ele_func_1 = x*2
 
-	end function ele_func_1
+    end function ele_func_1
 
-	elemental function ele_func_res(x) result(y)
-		integer,intent(in) :: x
-		integer :: y
+    elemental function ele_func_res(x) result(y)
+        integer,intent(in) :: x
+        integer :: y
 
-		y = x*2
+        y = x*2
 
-	end function ele_func_res
+    end function ele_func_res
 
-	elemental integer function ele_func_2(x, y)
-		integer,intent(in) :: x, y
+    elemental integer function ele_func_2(x, y)
+        integer,intent(in) :: x, y
 
-		ele_func_2 = x*2 + y*2
+        ele_func_2 = x*2 + y*2
 
-	end function ele_func_2
-
-
-	elemental subroutine ele_sub_2(x, y)
-		real(dp),intent(in) :: x
-		real(dp), intent(out) :: y
-
-		y = x*3
-
-	end subroutine ele_sub_2
-
-	subroutine call_ele()
-
-		integer :: x,x1(5),x2(5,5)
-
-		x = 1
-		x1 = 2
-		x2 = 2
-
-		x = ele_func_1(x)
-
-		x1 = ele_func_1(x1)
-
-		x2 = ele_func_1(x2)
+    end function ele_func_2
 
 
-	end subroutine call_ele
+    elemental subroutine ele_sub_2(x, y)
+        real(dp),intent(in) :: x
+        real(dp), intent(out) :: y
+
+        y = x*3
+
+    end subroutine ele_sub_2
+
+    subroutine call_ele()
+
+        integer :: x,x1(5),x2(5,5)
+
+        x = 1
+        x1 = 2
+        x2 = 2
+
+        x = ele_func_1(x)
+
+        x1 = ele_func_1(x1)
+
+        x2 = ele_func_1(x2)
+
+
+    end subroutine call_ele
 
 
     pure integer function pure_func(x)
         integer, intent(in) :: x
         pure_func = x
-	end function pure_func
+    end function pure_func
 
     impure integer function impure_func(x)
-	    integer, intent(in) :: x
+        integer, intent(in) :: x
         impure_func = x
-	end function impure_func
+    end function impure_func
 
 
 end module elements
--- tests/src/namelist.f90
+++ tests/src/namelist.f90
@@ -2,27 +2,25 @@
 
 module namelist
 
-	use iso_fortran_env, only: output_unit, real128
+    use iso_fortran_env, only: output_unit, real128
+    
+    implicit none
+    
+    ! Parameters
+    integer, parameter :: dp = selected_real_kind(p=15)
+    integer, parameter :: qp = selected_real_kind(p=30)
+    integer, parameter :: lp = selected_int_kind(16)
+    
+
+    integer :: a_int
+    real :: b_real
+    real(dp) :: b_real_dp
+    character(len=100) :: c_str
+
+    namelist /namelist1/ a_int, b_real, b_real_dp, c_str
+
+    
+    contains
 	
-	implicit none
-	
-	! Parameters
-	integer, parameter :: dp = selected_real_kind(p=15)
-	integer, parameter :: qp = selected_real_kind(p=30)
-	integer, parameter :: lp = selected_int_kind(16)
-	
-
-	integer :: a_int
-	real :: b_real
-	real(dp) :: b_real_dp
-	character(len=100) :: c_str
-
-	namelist /namelist1/ a_int, b_real, b_real_dp, c_str
-
-	
-	contains
-
-
-
 end module namelist
  
